feeb032794ad9c9474179a85becd64212fe127ba
fwsmit <fw.smit01@gmail.com>
2022-05-31 17:59:44 +0200

    Make the osm import use the node class as well

--- import_osm.py
+++ import_osm.py
@@ -3,10 +3,11 @@ from xml import sax
 from node import Node
 
 class OSMContentHandler(xml.sax.ContentHandler):
-    def __init__(self):
+    def __init__(self, nodes):
         xml.sax.ContentHandler.__init__(self)
         self.rwn_ref = None
         self.rcn_ref = None
+        self.nodes = nodes
 
     def startElement(self, name, attrs):
         print('start:', name)
@@ -30,7 +31,11 @@ class OSMContentHandler(xml.sax.ContentHandler):
         if name == "node":
             print('rwn_ref:', self.rwn_ref)
             print('rcn_ref:', self.rcn_ref)
+            self.nodes.append(Node(x=0, y=0, rwn_ref=self.rwn_ref, rcn_ref=self.rcn_ref))
+
 
 
 def import_osm(filename):
-    xml.sax.parse('data/best.osm', OSMContentHandler())
+    nodes = []
+    xml.sax.parse('data/best.osm', OSMContentHandler(nodes))
+    return nodes
--- knooppunten.py
+++ knooppunten.py
@@ -1,5 +1,7 @@
+from import_osm import import_osm
 
 def main():
+    import_osm("data/best.osm")
     print("Done")
 
 if __name__ == "__main__":
