b91573adf30b0b92a4dd8ea44c68fcc501b42346
fwsmit <fw.smit01@gmail.com>
2022-05-31 19:25:10 +0200

    compare: start working on simple compare algorithm

--- compare.py
+++ compare.py
@@ -15,3 +15,20 @@ def dist_simple_sq(lat1, lon1, lat2, lon2):
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     return dlon**2 + dlat**2
+
+
+def find_matching_point(node_ext, nodes_osm):
+    closest_node = None
+    best_dist_sq = math.inf
+    for node in nodes_osm:
+        if node.rwn_ref == node_ext.rwn_ref and node.rcn_ref == node_ext.rcn_ref:
+            dist_sq = dist_simple_sq(node.lat, node.lon, node_ext.lat, node_ext.lon)
+            if dist_sq < best_dist_sq:
+                best_dist_sq = dist_sq
+                closest_node = node
+
+    if closest_node:
+        actual_dist = dist_complicated(closest_node.lat, closest_node.lon, node_ext.lat, node_ext.lon)
+        print("Distance", actual_dist)
+
+    return closest_node
--- knooppunten.py
+++ knooppunten.py
@@ -1,17 +1,20 @@
 from import_osm import import_osm
 from import_geojson import import_geojson
+from compare import find_matching_point
 
 def main():
     nodes_osm = import_osm("data/best.osm")
     nodes_ext = import_geojson("data/groningen.geojson")
 
-    print("OSM nodes")
-    for n in nodes_osm:
-        print(n.__geo_interface__)
+    #  print("OSM nodes")
+    #  for n in nodes_osm:
+        #  print(n.__geo_interface__)
 
-    print("Geojson nodes")
-    for n in nodes_ext:
-        print(n.__geo_interface__)
+    #  print("Geojson nodes")
+    #  for n in nodes_ext:
+        #  print(n.__geo_interface__)
+
+    find_matching_point(nodes_ext[1], nodes_osm);
 
     print("Done")
 
