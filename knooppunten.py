ed907628e34793c0d36846d6fedb3e233d58ccde
fwsmit <fw.smit01@gmail.com>
2022-06-25 20:42:38 +0200

    Export great and poor matches to geojson as well

--- knooppunten.py
+++ knooppunten.py
@@ -16,9 +16,9 @@ def main():
     #  for n in nodes_ext:
         #  print(n.__geo_interface__)
 
-    great_matches = 0
-    good_matches = 0
-    poor_matches = 0
+    great_matches = []
+    good_matches = []
+    poor_matches = []
     non_matches = []
 
     for node in nodes_ext:
@@ -29,11 +29,11 @@ def main():
             dist = math.inf
 
         if dist < 1:
-            great_matches += 1
+            great_matches.append(node)
         elif dist < 10:
-            good_matches += 1
+            good_matches.append(node)
         elif dist < 100:
-            poor_matches += 1
+            poor_matches.append(node)
             #  print("Poor match. Distance:", dist)
         else:
             non_matches.append(node)
@@ -42,12 +42,14 @@ def main():
     print("Statistics:")
     print("Nodes analyzed (external):", len(nodes_ext))
     print("Nodes analyzed (OSM):", len(nodes_ext))
-    print("Great matches (<1m):", great_matches)
-    print("Good matches: (1-10m)", good_matches)
-    print("Poor matches: (10-100m)", poor_matches)
+    print("Great matches (<1m):", len(great_matches))
+    print("Good matches: (1-10m)", len(good_matches))
+    print("Poor matches: (10-100m)", len(poor_matches))
     print("Non matches: (>100m))", len(non_matches))
 
     export_geojson(non_matches, "non_matches.geojson")
+    export_geojson(poor_matches, "poor_matches.geojson")
+    export_geojson(good_matches, "good_matches.geojson")
 
 if __name__ == "__main__":
     main()
