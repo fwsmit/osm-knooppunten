541e0eb5f143f6976ae5f527d26956adc157b4bb
Friso Smit <fw.smit01@gmail.com>
2022-08-21 21:04:16 +0200

    GUI: Add function for adding file selector

--- gui.py
+++ gui.py
@@ -4,20 +4,26 @@ from PySide6 import QtCore, QtWidgets, QtGui
 from pathlib import Path
 
 class MainWindow(QtWidgets.QWidget):
+    def addFileSlot(self, fileSelectFunc, label):
+        button = QtWidgets.QPushButton(label)
+        text = QtWidgets.QLineEdit()
+
+        hlayout = QtWidgets.QHBoxLayout(self)
+        hlayout.addWidget(text)
+        hlayout.addWidget(button)
+
+        button.clicked.connect(self.selectOSM)
+
+        return text, button
+
+
     def __init__(self):
         super().__init__()
 
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-        self.button = QtWidgets.QPushButton("Select OSM file")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.text1, button1 = self.addFileSlot(self.selectOSM, "Select OSM file")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.selectOSM)
 
 
     @QtCore.Slot()
@@ -27,7 +33,7 @@ class MainWindow(QtWidgets.QWidget):
                 filter="All Files (*);;OSM Files (*.osm)",
                 selectedFilter="OSM Files (*.osm)")
 
-        self.text.setText(self.osmFile)
+        self.text1.setText(self.osmFile)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
