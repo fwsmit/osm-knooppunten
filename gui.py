171e461a9ccae0a6097596aacd8e42cef8385546
Friso Smit <fw.smit01@gmail.com>
2022-09-14 16:42:58 +0200

    Add run window to the gui
    The process is also executed in parallel

--- gui.py
+++ gui.py
@@ -1,5 +1,56 @@
 from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import QRunnable
 from analyze import do_analysis
+
+def gui_do_analysis(osmFileName, importFile, filterRegion):
+        with open(str(osmFileName), 'r') as osmFile:
+            do_analysis(osmFile, importFile, filterRegion)
+
+
+class Worker(QRunnable):
+    '''
+    Worker thread
+
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    :param callback: The function callback to run on this worker thread. Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
+
+    '''
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    @QtCore.Slot()
+    def run(self):
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+        self.fn(*self.args, **self.kwargs)
+
+class RunWindow(QtWidgets.QWidget):
+    def __init__(self, prnt, osmFileName, importFile, filterRegion):
+        super().__init__(parent=None)
+        print("started run window")
+        self.osmFileName = osmFileName
+        self.importFile = importFile
+        self.filterRegion = filterRegion
+        self.setWindowTitle("Running analysis...")
+        vlayout = QtWidgets.QVBoxLayout(self)
+        label = QtWidgets.QLabel(osmFileName)
+        vlayout.addWidget(label)
+
+        self.threadpool = QtCore.QThreadPool()
+
+        worker = Worker(gui_do_analysis, osmFileName, importFile, filterRegion)
+        self.threadpool.start(worker)
 
 class MainWindow(QtWidgets.QWidget):
     def addFileSlot(self, fileSelectFunc, label, layout):
@@ -90,5 +141,7 @@ class MainWindow(QtWidgets.QWidget):
         if self.importFile is None:
             return -1
 
-        with open(str(self.osmFile), 'r') as osmFile:
-            do_analysis(osmFile, self.importFile, filterRegion)
+        self.runWindow = RunWindow(self, self.osmFile, self.importFile, self.filterRegion)
+        self.runWindow.resize(600, 400)
+        self.runWindow.show()
+
