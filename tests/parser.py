340dcb9a8d418c1f89bdc0a0130b6011a11a6444
Friso Smit <fw.smit01@gmail.com>
2022-10-17 16:53:14 +0200

    Make everything a module to make running tests easier

---
+++ tests/parser.py
@@ -0,0 +1,7 @@
+import unittest
+import import_osm
+
+class TestImport(unittest.TestCase):
+
+    def test_osm(self):
+        pass
--- tests/runner.py
+++ tests/runner.py
@@ -1,12 +1,14 @@
 import unittest
-import analyze
+import tests.analyze
+import tests.parser
 
 # initialize the test suite
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(analyze))
+suite.addTests(loader.loadTestsFromModule(tests.analyze))
+suite.addTests(loader.loadTestsFromModule(tests.parser))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
