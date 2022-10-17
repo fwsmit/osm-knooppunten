9e4d012b24eebbe92bcecc8fea46d28867ceaf58
Friso Smit <fw.smit01@gmail.com>
2022-10-17 17:49:33 +0200

    Add tests for the parser

--- tests/parser.py
+++ tests/parser.py
@@ -1,7 +1,13 @@
 import unittest
-import import_osm
+from import_osm import import_osm
 
 class TestImport(unittest.TestCase):
 
     def test_osm(self):
-        pass
+        nodes = import_osm("tests/data/test.osm")
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].lat, 53.23)
+        self.assertEqual(nodes[0].rwn_ref, '82')
+        self.assertEqual(nodes[0].rcn_ref, '89')
+        # self.assertEqual(nodes[1].rcn_ref, None)
+        self.assertEqual(nodes[2].lon, 6.53)
