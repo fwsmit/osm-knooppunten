b4f8ff3f1224233408fdcd550fa00cab8bb93533
fwsmit <fw.smit01@gmail.com>
2022-12-19 10:28:47 +0100

    Change working directory to executable location
    On Windows it would sometimes set the working directory to a system
    directory. This would make the script fail.
    
    A better solution would be to prepend all relative paths with this root
    path.

--- knooppunten.py
+++ knooppunten.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse as arg
 from PySide6 import QtWidgets
@@ -5,6 +6,9 @@ from _version import __version__
 from gui import MainWindow
 
 def main():
+    rootpath = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(rootpath)
+    print("Working directory:", os.getcwd())
     parser = arg.ArgumentParser()
     parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=__version__))
     parser.parse_args()
